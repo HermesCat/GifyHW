9a2e268a4769d669edb16d4c5c61d952380d2806
HermesCat <jppettis@comcast.net>
2019-03-23 01:08:58 -0400

    fixed an issues

--- app.js
+++ app.js
@@ -27,46 +27,28 @@ function renderButtons() {
 //...use the buttons to search GIPHY API.......................................................
 
 $(".animal").on("click", function() {
-    // Grabbing and storing the data-animal property value from the button
     $("#imageDisplay").empty();
     var animal = $(this).attr("data-name");
     console.log(this);
 
-    // Constructing a queryURL using the animal name
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     animal + "&api_key=TIYiADDeCV4ZlO6n3oUTGqMbl0VvM7xC&limit=10&fixed_height_small_still";
 
-    // Performing an AJAX request with the queryURL
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      // After data comes back from the request
-      .then(function(response) {
-        // console.log(queryURL);
+      .then(function(response) {;
         console.log(response);
-        // storing the data from the AJAX request in the results variable
         var results = response.data;
-
-        // Looping through each result item
+//...make a for loop to append the animal images to the DOM.......................................
         for (var i = 0; i < results.length; i++) {
-
-          // Creating and storing a div tag
           var animalDiv = $("<div>");
-
-          // Creating a paragraph tag with the result item's rating
           var p = $("<p>").text("Rating: " + results[i].rating);
-
-          // Creating and storing an image tag
           var animalImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
           animalImage.attr("src", results[i].images.fixed_height.url);
-
-          // Appending the paragraph and image tag to the animalDiv
           animalDiv.append(p);
           animalDiv.append(animalImage);
-
-          // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
           $("#imageDisplay").prepend(animalDiv);
         }
       });
